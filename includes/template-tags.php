b6103c5aa5d5e6ee0ba716bd1a3a4fa0668a4243
Laurens Offereins <lmoffereins@gmail.com>
2015-12-07 17:51:24 +0100

    Move `vgsr_entity_get_meta()` to template-tags.php

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -10,6 +10,35 @@
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Return the entity post meta
+ *
+ * @since 1.1.0
+ *
+ * @uses is_entity()
+ * @uses VGSR_Entity_Base::get_meta()
+ * @uses apply_filters() Calls 'vgsr_entity_get_meta'
+ *
+ * @param int|WP_Post $post Optional. Post ID or object
+ * @return array Array with entity meta. Empty array when post is not an entity.
+ */
+function vgsr_entity_get_meta( $post = 0 ) {
+
+	// Get the post
+	$post = get_post( $post );
+
+	// Bail when this is not an entity
+	if ( is_entity( $post ) )
+		return array();
+
+	$type = get_post_type( $post );
+	$meta = vgsr_entity()->{$type}->get_meta();
+
+	return apply_filters( 'vgsr_entity_get_meta', $meta, $post );
+}
+
+/** Is *****************************************************************/
+
 if ( ! function_exists( 'is_entity' ) ) :
 /**
  * Return whether the post is an entity
@@ -82,7 +111,7 @@ function is_entity_parent( $post = 0 ) {
 	if ( ! $post = get_post( $post ) )
 		return false;
 
-	// Find this post
+	// Find this post as a parent
 	$post_type = array_search( $post->ID, vgsr_entity()->get_entity_parents() );
 
 	return ( post_type_exists( $post_type ) ? $post_type : false );
