6a005551587a376db0ed474b642262064c9dc0e5
Laurens Offereins <lmoffereins@gmail.com>
2015-12-08 16:25:39 +0100

    Introduce `dissect_pow2()` to find power of 2 values in a number

--- includes/functions.php
+++ includes/functions.php
@@ -9,3 +9,24 @@
 
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
+
+if ( ! function_exists( 'dissect_pow2' ) ) :
+/**
+ * Return all power of 2 values that are in the value
+ *
+ * @since 1.1.0
+ *
+ * @param int $value Value to dissect
+ * @return array Values of power of 2 found
+ */
+function dissect_pow2( $value = 0 ) {
+	$retval = array();
+	if ( is_numeric( $value ) && (int) $value > 0 ) {
+		foreach ( array_reverse( str_split( (string) decbin( (int) $value ) ) ) as $pow => $bi ) {
+			if ( $bi ) $retval[] = pow( 2, $pow );
+		}
+	}
+
+	return $retval;
+}
+endif;
