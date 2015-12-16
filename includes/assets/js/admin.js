2c992c907a99b4793404430d1581fb3b92e49387
Laurens Offereins <lmoffereins@gmail.com>
2015-12-16 16:05:26 +0100

    Enable /assets/js/admin.js to run on not-edit.php pages

--- includes/assets/js/admin.js
+++ includes/assets/js/admin.js
@@ -5,6 +5,7 @@
  * @subpackage Administration
  */
 
+/* global inlineEditPost, entityEditPost */
 ( function( $ ) {
 
 	// Can I use datepicker? Initiate datepickers
@@ -24,41 +25,43 @@
 	var $inline = $( '.inline-edit-row' );
 	$inline.find( '.entity-quick-edit .inline-edit-col' ).appendTo( $inline.find( '.inline-edit-col-right' ).first() );
 
-	/* global inlineEditPost, entityEditPost */
+	// When editing inline
+	if ( typeof inlineEditPost !== 'undefined' ) {
 
-	// Create a copy of the WP inline edit post function
-	var wp_inline_edit = inlineEditPost.edit;
+		// Create a copy of the WP inline edit post function
+		var wp_inline_edit = inlineEditPost.edit;
 
-	// Overwrite the function with our own code
-	inlineEditPost.edit = function( id ) {
+		// Overwrite the function with our own code
+		inlineEditPost.edit = function( id ) {
 
-		// Run the original inline edit function
-		wp_inline_edit.apply( this, arguments );
+			// Run the original inline edit function
+			wp_inline_edit.apply( this, arguments );
 
-		var t = this, $editRow, $rowData, fields, val;
+			var t = this, $editRow, $rowData, fields, val;
 
-		if ( typeof( id ) === 'object' ) {
-			id = t.getId( id );
-		}
+			if ( typeof( id ) === 'object' ) {
+				id = t.getId( id );
+			}
 
-		$editRow = $( '.inline-editor' );
-		$rowData = $( '#post-' + id );
+			$editRow = $( '.inline-editor' );
+			$rowData = $( '#post-' + id );
 
-		// Can I use datepicker? Initiate datepickers inside the edit row
-		if ( $.fn.datepicker ) {
-			$editRow.find( '.input-text-wrap .datepicker' ).datepicker( dp_args );
-		}
+			// Can I use datepicker? Initiate datepickers inside the edit row
+			if ( $.fn.datepicker ) {
+				$editRow.find( '.input-text-wrap .datepicker' ).datepicker( dp_args );
+			}
 
-		fields = entityEditPost.fields;
+			fields = entityEditPost.fields;
 
-		// Refresh input values for this post
-		for ( f = 0; f < fields.length; f++ ) {
-			val = $( '.' + fields[ f ].key + ' .edit-value', $rowData );
-			// Deal with Twemoji
-			val.find( 'img' ).replaceWith( function() { return this.alt; } );
-			val = val.text();
-			$( ':input[name="' + fields[ f ].name + '"]', $editRow ).val( val );
-		}
-	};
+			// Refresh input values for this post
+			for ( f = 0; f < fields.length; f++ ) {
+				val = $( '.' + fields[ f ].key + ' .edit-value', $rowData );
+				// Deal with Twemoji
+				val.find( 'img' ).replaceWith( function() { return this.alt; } );
+				val = val.text();
+				$( ':input[name="' + fields[ f ].name + '"]', $editRow ).val( val );
+			}
+		};
+	}
 
 })( jQuery );
