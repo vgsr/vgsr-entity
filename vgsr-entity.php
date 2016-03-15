95c4ac9bf838eafb48705d17503eef4ca0a391d1
Laurens Offereins <lmoffereins@gmail.com>
2016-03-15 15:24:54 +0100

    Update to version 2.0.0-alpha-2

--- vgsr-entity.php
+++ vgsr-entity.php
@@ -13,7 +13,7 @@
  * Plugin URI:        https://github.com/vgsr/vgsr-entity
  * Author:            Laurens Offereins
  * Author URI:        https://github.com/lmoffereins
- * Version:           2.0.0-alpha
+ * Version:           2.0.0-alpha-2
  * Text Domain:       vgsr-entity
  * Domain Path:       /languages/
  * GitHub Plugin URI: vgsr/vgsr-entity
@@ -89,7 +89,7 @@ final class VGSR_Entity {
 
 		/** Versions **********************************************************/
 
-		$this->version       = '2.0.0-alpha';
+		$this->version       = '2.0.0-alpha-2';
 
 		/** Paths *************************************************************/
 
