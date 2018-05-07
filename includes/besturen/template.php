3e6ecd960b33de267157128d7ee66d587296c361
Laurens Offereins <lmoffereins@gmail.com>
2018-05-07 08:10:34 +0200

    Bestuur: enable returning the full post object for `vgsr_entity_get_current_bestuur()`

--- includes/besturen/template.php
+++ includes/besturen/template.php
@@ -16,10 +16,26 @@ defined( 'ABSPATH' ) || exit;
  * @since 2.0.0
  *
  * @uses apply_filters() Calls 'vgsr_entity_get_current_bestuur'
- * @return int Post ID
+ *
+ * @param bool $object Optional. Whether to return as a post object. Defaults to false.
+ * @return int|WP_Post|false Post ID or object or False when not found
  */
-function vgsr_entity_get_current_bestuur() {
-	return (int) apply_filters( 'vgsr_entity_get_current_bestuur', get_option( '_bestuur-latest-bestuur' ) );
+function vgsr_entity_get_current_bestuur( $object = false ) {
+
+	// Get post setting and validate
+	$post  = (int) get_option( '_bestuur-latest-bestuur' );
+	$_post = get_post( $post );
+
+	// Get post object
+	if ( $_post && $object ) {
+		$post = $_post;
+
+	// Default false for invalid post
+	} elseif ( ! $_post ) {
+		$post = false;
+	}
+
+	return apply_filters( 'vgsr_entity_get_current_bestuur', $post, $object );
 }
 
 /**
