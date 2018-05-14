aea6102d0bdda2c9ee0bc9d7dc8e720801c66dec
Laurens Offereins <lmoffereins@gmail.com>
2018-05-14 07:30:19 +0200

    BuddyPress: split Kast logic into dedicated files

---
+++ includes/extend/buddypress/actions.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * VGSR Entity BuddyPress Actions
+ *
+ * @package VGSR Entity
+ * @subpackage BuddyPress
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/** Kast **********************************************************************/
+
+add_filter( 'bp_xprofile_get_groups',         'vgsr_entity_bp_filter_kast_address_profile_groups_fields',  5, 2 );
+add_filter( 'bp_get_the_profile_field_value', 'vgsr_entity_bp_kast_address_profile_field_value',          10, 3 );
+add_filter( 'bp_get_member_profile_data',     'vgsr_entity_bp_kast_address_profile_field_data',           10, 2 );
+add_filter( 'bp_get_profile_field_data',      'vgsr_entity_bp_kast_address_profile_field_data',           10, 2 );
