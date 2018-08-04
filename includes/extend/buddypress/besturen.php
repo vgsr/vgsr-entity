652066f6f731b909a5a9d7064c0166610ae57e6c
Laurens Offereins <lmoffereins@gmail.com>
2018-08-04 16:54:49 +0200

    BuddyPress: move logic for Besturen to separate file

---
+++ includes/extend/buddypress/besturen.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * VGSR Entity BuddyPress Bestuur Functions
+ *
+ * @package VGSR Entity
+ * @subpackage BuddyPress
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/** Details *******************************************************************/
+
+/**
+ * Modify the Bestuur's position name to link to the member's profile
+ *
+ * @since 2.0.0
+ *
+ * @param string $name Displayed name
+ * @param WP_User|bool $user User object or False when not found
+ * @param array $args Bestuur position arguments
+ */
+function vgsr_entity_bp_bestuur_position_name( $name, $user, $args ) {
+
+	// For existing users
+	if ( is_a( $user, 'WP_User' ) ) {
+
+		// Collect template global. Might not exist yet.
+		global $members_template;
+		$_members_template = $members_template;
+
+		/**
+		 * Use BP member loop for using template tags
+		 *
+		 * Setting up the template loop per member is really
+		 * not efficient, but for now it does the job.
+		 */
+		if ( bp_has_members( array( 'type' => '', 'include' => $user->ID ) ) ) :
+			while ( bp_members() ) : bp_the_member();
+
+				// Member profile link
+				$name = sprintf( '<a href="%s">%s</a>', bp_get_member_permalink(), bp_get_member_name() );
+			endwhile;
+		endif;
+
+		// Reset global
+		$members_template = $_members_template;
+	}
+
+	return $name;
+}
--- includes/extend/buddypress/buddypress.php
+++ includes/extend/buddypress/buddypress.php
@@ -76,6 +76,7 @@ class VGSR_Entity_BuddyPress {
 
 		// Core
 		require( $this->includes_dir . 'actions.php'   );
+		require( $this->includes_dir . 'besturen.php'  );
 		require( $this->includes_dir . 'functions.php' );
 		require( $this->includes_dir . 'kasten.php'    );
 		require( $this->includes_dir . 'members.php'   );
@@ -253,49 +254,8 @@ class VGSR_Entity_BuddyPress {
 		if ( vgsr_entity_check_access() ) {
 
 			// Bestuur: replace position name
-			add_action( 'vgsr_entity_bestuur_position_name', array( $this, 'bestuur_position_name' ), 10, 3 );
+			add_filter( 'vgsr_entity_bestuur_position_name', 'vgsr_entity_bp_bestuur_position_name', 10, 3 );
 		}
-	}
-
-	/** Details ***************************************************************/
-
-	/**
-	 * Modify the Bestuur's position name to link to the member's profile
-	 *
-	 * @since 2.0.0
-	 *
-	 * @param string $name Displayed name
-	 * @param WP_User|bool $user User object or False when not found
-	 * @param array $args Bestuur position arguments
-	 */
-	public function bestuur_position_name( $name, $user, $args ) {
-
-		// For existing users
-		if ( is_a( $user, 'WP_User' ) ) {
-
-			// Collect template global. Might not exist yet.
-			global $members_template;
-			$_members_template = $members_template;
-
-			/**
-			 * Use BP member loop for using template tags
-			 *
-			 * Setting up the template loop per member is really
-			 * not efficient, but for now it does the job.
-			 */
-			if ( bp_has_members( array( 'type' => '', 'include' => $user->ID ) ) ) :
-				while ( bp_members() ) : bp_the_member();
-
-					// Member profile link
-					$name = sprintf( '<a href="%s">%s</a>', bp_get_member_permalink(), bp_get_member_name() );
-				endwhile;
-			endif;
-
-			// Reset global
-			$members_template = $_members_template;
-		}
-
-		return $name;
 	}
 }
 
