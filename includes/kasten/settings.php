599c5266d77fb6791ded88073d80404674da770d
Laurens Offereins <lmoffereins@gmail.com>
2018-08-04 15:25:30 +0200

    Kast: remove Recreate Thumbnails setting field

--- includes/kasten/settings.php
+++ includes/kasten/settings.php
@@ -19,30 +19,5 @@ defined( 'ABSPATH' ) || exit;
  * @return array Settings fields
  */
 function vgsr_entity_kast_settings_fields() {
-	return (array) apply_filters( 'vgsr_entity_kast_settings_fields', array(
-
-		// Thumbnails
-		'downsize-thumbs' => array(
-			'title'             => esc_html__( 'Recreate Thumbnails', 'vgsr-entity' ),
-			'callback'          => 'vgsr_entity_kast_settings_downsize_thumbs_field',
-			'sanitize_callback' => 'intval',
-			'entity'            => 'kast',
-			'section'           => 'main',
-			'args'              => array(),		
-		)
-	) );
-}
-
-/**
- * Output the Kast downsize thumbs settings field
- *
- * @since 1.0.0
- */
-function vgsr_entity_kast_settings_downsize_thumbs_field() { ?>
-
-	<input type="checkbox" name="_kast-downsize-thumbs" id="_kast-downsize-thumbs" <?php checked( get_option( '_kast-downsize-thumbs' ) ); ?> value="1" />
-
-	<p class="description"><?php esc_html_e( 'This is a one time resizing of thumbs for Kasten. NOTE: This option only adds new image sizes, it does not remove old ones.', 'vgsr-entity' ); ?></p>
-
-	<?php
+	return (array) apply_filters( 'vgsr_entity_kast_settings_fields', array() );
 }
